a727cabebbc65a92c09f9bf11d029f18687362d9
lealre <renaanvp@gmail.com>
2024-02-22 20:06:21 +0000

    done

---
+++ 01/kpi.py
@@ -0,0 +1,8 @@
+
+name = input('Name: ')
+salary = input('Salary: ')
+bonus = input('Bonus: ')
+
+bonus_value = float(salary) * float(bonus)
+
+print(f'Hello {name}! Your bonus value is {bonus_value}')
