82226653eaf1b656d936f3aeccd95d81b1c681d7
lealre <renaanvp@gmail.com>
2024-02-22 20:10:09 +0000

    correct structure

--- 01/kpi.py
+++ 01/kpi.py
@@ -3,6 +3,6 @@ name = input('Name: ')
 salary = input('Salary: ')
 bonus = input('Bonus: ')
 
-bonus_value = float(salary) * float(bonus)
+bonus_value = 1000 + float(salary) * float(bonus)
 
 print(f'Hello {name}! Your bonus value is {bonus_value}')
