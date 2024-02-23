7d5efa2748479c2c85915bdba9a49bac8764723b
lealre <renaanvp@gmail.com>
2024-02-23 22:46:00 +0000

    finish script

---
+++ 02/kpi.py
@@ -0,0 +1,37 @@
+
+# name
+try:
+    name = input('Name: ')
+    if len(name) == 0:
+        print('The name field is empty')
+        exit()
+except ValueError as e:
+    print(e)
+
+
+# salary
+try:
+    salary = input('Salary: ')
+    salary = float(salary)
+    if salary < 0:
+        print('Number cannot be negative')
+        exit()
+except ValueError:
+    print('Must be a number')
+    exit()
+
+
+# bonus
+try:
+    bonus = input('Bonus: ')
+    bonus = float(bonus)
+    if bonus < 0:
+        print('Number cannot be negative')
+        exit()
+except ValueError:
+    print('Must be a number')
+    exit()
+
+# kpi
+bonus_value = 1000 + salary * bonus
+print(f'Hello {name}! Your bonus value is {bonus_value}')
