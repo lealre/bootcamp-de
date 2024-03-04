6c2298255845e25504fe8200edc7e9b741bbe7f6
lealre <renaanvp@gmail.com>
2024-03-04 21:09:08 +0000

    update

--- 09/example.py
+++ 09/example.py
@@ -1,5 +1,12 @@
 from loguru import logger
 
+# create a log_file.log
+logger.add(
+    '09/log_file.log',
+    format =  "{time} <r>{level}</r> <g>{message}</g> {file}"
+)
+
+
 logger.info('Initialization processes, routine transactions, ...') 
 logger.debug('Workflow applications') 
 logger.warning('Situations that need attention but do not stop the system from working') 
