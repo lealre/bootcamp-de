3c14900a3f99dec3bd1b7503de188f4b26313775
lealre <renaanvp@gmail.com>
2024-03-04 20:09:05 +0000

    examples of log funcionalitie

---
+++ 09/example.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.info('Initialization processes, routine transactions, ...') 
+logger.debug('Workflow applications') 
+logger.warning('Situations that need attention but do not stop the system from working') 
+logger.error('Some system operations failed')
+logger.critical('Stop system operation')
